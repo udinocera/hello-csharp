3d8beb68e2bd04218a03044bcb320291bd1aa131
udinocera <udinocera@gmail.com>
2022-03-31 17:24:23 +0200

    Fine

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//prova
+
+Console.WriteLine("Salve, puoi scrivere il tuo mese preferito?");
+
+Console.WriteLine("Gennaio");
+Console.WriteLine("Febbraio");
+Console.WriteLine("Marzo");
+Console.WriteLine("Aprile");
+Console.WriteLine("Maggio");
+Console.WriteLine("Giugno");
+Console.WriteLine("Luglio");
+Console.WriteLine("Agosto");
+Console.WriteLine("Settembre");
+Console.WriteLine("Ottobre");
+Console.WriteLine("Novembre");
+Console.WriteLine("Dicembre");
+
+var mesi = Console.ReadLine();
+
+Console.WriteLine("Il mese che hai scelto è ");
+Console.WriteLine(mesi);
